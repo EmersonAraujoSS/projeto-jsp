2d6f6015b91db0b01d5c76d241e2c444d992278a
emerson.araujo <mcemersonaraujo@gmail.com>
2024-04-22 16:21:18 -0400

    criação da classe gitteste

---
+++ src/main/java/model/gitteste.java
@@ -0,0 +1,32 @@
+package model;
+
+public class gitteste {
+
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
